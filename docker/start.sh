739c92a18a40fedb18e3a41ed2b890c5e3ae9862
592767809 <592767809@qq.com>
2025-10-22 15:33:31 +0800

    更新

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,4 @@
 
 docker builder prune -f
 docker build --no-cache -t python:file_manager .
-docker run -itd --name file_manager -p 5780:80 --restart=always -v /mnt/vdb1:/srv python:file_manager
+docker run -itd --name file_manager --net=host --restart=always -v /mnt/vdb1:/srv python:file_manager
