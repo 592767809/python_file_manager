f58035e914cd31a44b30da242565d38f30e42466
592767809 <592767809@qq.com>
2025-10-22 15:29:34 +0800

    更新

--- server/urls.py
+++ server/urls.py
@@ -1,13 +1,8 @@
 
-from django.contrib import admin
-from django.urls import path, re_path
 from server import views
-from django.views.static import serve
-from django.conf import settings
+from django.urls import path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('file', views.file_manager),
-    re_path('^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
