f9760e090f5c4b54416c7f313019c6f56e44525e
592767809 <592767809@qq.com>
2025-10-22 14:59:13 +0800

    更新

--- server/views.py
+++ server/views.py
@@ -1,7 +1,8 @@
 
 import os
-from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed, HttpResponseNotFound, FileResponse, HttpResponseForbidden
+from urllib import parse
 from apikey.models import ApiKey
+from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed, HttpResponseNotFound, FileResponse, HttpResponseForbidden
 
 
 def file_manager(http_request: HttpRequest):
@@ -14,13 +15,15 @@ def file_manager(http_request: HttpRequest):
     if '..' in file_path:
         return HttpResponseNotFound()
     file_path = '/srv/' + file_path
+    base_path = os.path.dirname(file_path)
+    file_name = parse.unquote(os.path.basename(file_path))
+    file_path = os.path.join(base_path, file_name)
     if http_request.method == "GET":
         if os.path.exists(file_path):
             return FileResponse(open(file_path, 'rb'))
         else:
             return HttpResponseNotFound()
     elif http_request.method == "POST":
-        base_path = os.path.dirname(file_path)
         if not os.path.exists(base_path):
             os.makedirs(base_path)
         with open(file_path, 'wb') as f:
